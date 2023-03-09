e912a9ce8dba359467cc68a2dcb90eee9baa37d5
Bartosz Piechowiak <piechowiaq@gmail.com>
2023-03-09 09:00:30 +0100

    Refactoring validation

--- models/DVD.php
+++ models/DVD.php
@@ -11,13 +11,31 @@ class DVD extends Product
         $this->attributes = $attributes;
     }
 
+    public function validate()
+    {
+        parent::validate(); // Run parent validation rules
+
+        // Add additional validation rules for Product class
+        $attributes = $this->attributes;
+
+        foreach ($attributes as $key => $value) {
+            if (!($value)) {
+                $errors[$key] = 'Please, submit required '.$key;
+            }
+        }
+    }
+
     public function getAttributes()
     {
         return "Size: " . $this->attributes['size'] . " MB";
     }
 
-    public function rules()
+    public function rules(): array
     {
-        // TODO: Implement rules() method.
+        return [
+            parent::rules(),
+            'attributes' => [self::RULE_REQUIRED],
+
+        ];
     }
 }
--- models/Product.php
+++ models/Product.php
@@ -2,10 +2,11 @@
 
 namespace app\models;
 
-use app\core\Model;
-
-abstract class Product extends Model
+abstract class Product
 {
+    public const RULE_REQUIRED = 'required';
+    public const RULE_UNIQUE = 'unique';
+    public const RULE_INT = 'int';
 
     protected string $sku;
     protected string $name;
@@ -16,7 +17,6 @@ abstract class Product extends Model
         $this->sku = $sku;
         $this->name = $name;
         $this->price = $price;
-
     }
 
     public function getSku(): string
@@ -35,4 +35,49 @@ abstract class Product extends Model
     }
 
     abstract public function getAttributes();
+
+    public function rules(): array
+    {
+        return [
+            'sku' => [self::RULE_REQUIRED],
+            'name' => [self::RULE_REQUIRED],
+            'price' => [self::RULE_REQUIRED],
+        ];
+    }
+
+    public function validate()
+    {
+        foreach ($this->rules() as $attribute => $rules){
+            $value = $this->{$attribute};
+            foreach ($rules as $rule){
+                $ruleName = $rule;
+                if(!is_string($ruleName)){
+                    $ruleName = $rule[0];
+                }
+                if ($ruleName === self::RULE_REQUIRED && !$value) {
+                    $this->addErrorForRule($attribute, self::RULE_REQUIRED);
+                }
+
+            }
+        }
+        return empty($this->errors);
+    }
+
+    private function addErrorForRule(string $attribute, string $rule, $params = [])
+    {
+        $message = $this->errorMessages()[$rule] ?? '';
+
+        foreach ($params as $key => $value) {
+            $message = str_replace("{{$key}}", $value, $message);
+        }
+        $this->errors[$attribute][] = $message;
+    }
+
+    public function errorMessages(): array
+    {
+        return [
+            self::RULE_REQUIRED => 'This field is required',
+            self::RULE_UNIQUE => 'Record with this {field} already exist',
+        ];
+    }
 }
